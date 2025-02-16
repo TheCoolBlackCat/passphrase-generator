670871fa278979a76693099b1d975a21ce13b7b7
Timothy Clark <git@tdhc.uk>
2025-02-16 16:39:48 +0000

    Update GIFs

---
+++ res/generate-gifs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FONT_FAMILY="BitstromWera Nerd Font"
+THEME="asciinema"
+FONT_SIZE=100
+SPEED=2
+
+agg --font-family "$FONT_FAMILY" --theme $THEME --font-size $FONT_SIZE --speed $SPEED demo1.cast demo1.gif
+agg --font-family "$FONT_FAMILY" --theme $THEME --font-size $FONT_SIZE --speed $SPEED demo2.cast demo2.gif
