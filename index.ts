f1e481ef64c70c943f10f20100aabb4c05ab65df
Timothy Clark <git@tdhc.uk>
2025-02-16 14:30:06 +0000

    Add CLI args

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-import prompts from "prompts";
+import prompts, { Answers, PromptObject } from "prompts";
 import fs from "fs/promises";
 import { info, log } from "console";
+import { Command } from 'commander';
 
 
 interface WordList {
@@ -10,6 +11,7 @@ interface WordList {
     wordsByLength: Record<number, Array<string>>;
     stats: {
         shortestWordLength: number
+        availableWordSizes: Array<number>
     }
 }
 
@@ -35,8 +37,9 @@ async function migrateWordsList() {
         shortestWordLength = Math.min(shortestWordLength, length)
         return acc;
     }, {});
+    const availableWordSizes = Object.keys(wordsByLength).map(key => Number(key)).sort((a, b) => b - a)
   
-    const jsonWords = JSON.stringify({words, wordsByLength, stats: {shortestWordLength}} satisfies WordList);
+    const jsonWords = JSON.stringify({words, wordsByLength, stats: {shortestWordLength, availableWordSizes}} satisfies WordList);
     await fs.writeFile("words.json", jsonWords);
     info("Words list migrated successfully");
 }
@@ -59,24 +62,24 @@ async function generatePassphrase({words, wordsByLength, stats}: WordList, lengt
         throw new Error(`Password length can't be shorter than the length of the shortest word, which is ${stats.shortestWordLength} characters long.`)
     }
     const numberOfSeperators = wordCount - 1
-    const targetLengthWithoutSeperators = length - (numberOfSeperators*separator.length) - (numberToJoin ? numberToJoin.toString().length : 0)
-    const minWordLength = Math.floor(targetLengthWithoutSeperators / wordCount)
-    console.log(`length=${length}, wordCount=${wordCount}, wordsByLength[${minWordLength}]`)
+    const targetLengthWithoutSeperators = length
+        - (numberOfSeperators*separator.length)
+        - (numberToJoin ? numberToJoin.toString().length : 0)
 
-    const availableWordLengths = Object.keys(wordsByLength).map(key => Number(key)).sort((a, b) => b - a)
-    console.log('availableWordLengths:', availableWordLengths)
+    // console.log(`length=${length}, wordCount=${wordCount}, wordsByLength[${minWordLength}]`)
+    // console.log('availableWordSizes:', stats.availableWordSizes)
 
     let generatedLength = 0
-    let lengthToFill = -1
+    let lengthToFill = 0
     let pickedWords: Array<string> = []
     let needToAddNumber = !!numberToJoin
     do {
-        const wordLength = availableWordLengths.reduce((currentLength: number, wordLength: number) => {
+        const wordLength = stats.availableWordSizes.reduce((currentLength: number, wordLength: number) => {
             if (wordLength <= lengthToFill && wordLength > currentLength) {
                 return wordLength
             }
             return currentLength
-        }, availableWordLengths[0])
+        }, stats.availableWordSizes[0])
         const randomWord = pickWordRandomly(wordsByLength[wordLength])
         const shouldAddNumberThisTime = needToAddNumber && (Math.random() > 0.5)
         if (shouldAddNumberThisTime) {
@@ -95,45 +98,69 @@ async function generatePassphrase({words, wordsByLength, stats}: WordList, lengt
 }
 
 async function main() {
-    // await migrateWordsList();
+    const program = new Command();
+    program
+        .option('-l, --length <number>', 'length of passphrase', '20')
+        .option('-s, --separator <char>', 'separator between words', '-')
+        .option('-c, --capitalise <boolean>', 'capitalize each word', 'true')
+        .option('-n, --add-number <boolean>', 'add random number', 'true')
+        .option('-i, --interactive', 'use interactive prompt mode')
+        .option('--migrate', 'migrate eff_wordlist.txt file to JSON');
 
-  const response = await prompts([
-    {
-      type: "number",
-      name: "length",
-      message: "What is the length restriction on your password?",
-      initial: 20,
-      min: 4,
-    },
-    {
-      type: "text",
-      name: "separator",
-      message: "What separator would you like to use?",
-      initial: "-",
-    },
-    {
-      type: "confirm",
-      name: "capitalise",
-      message: "Would you like to capitalise each word?",
-      initial: true,
-    },
-    {
-        type: "confirm",
-        name: "addNumber",
-        message: "Would you like to add a random number in the string?",
-        initial: true,
-      },
-  ]);
+    program.parse();
 
-  if (!response.length) {
-    console.log("Operation cancelled");
-    process.exit(1);
-  }
+    let options: Answers<'length' | 'separator' | 'capitalise' | 'addNumber'>;
+    
+    if (program.opts().migrate) {
+        return await migrateWordsList();
+    }
+    else if (program.opts().interactive || process.argv.length === 2) {
+        options = await prompts([
+            {
+                type: "number",
+                name: "length",
+                message: "What is the length restriction on your password?",
+                initial: 20,
+                min: 4,
+              },
+              {
+                type: "text",
+                name: "separator",
+                message: "What separator would you like to use?",
+                initial: "-",
+              },
+              {
+                type: "confirm",
+                name: "capitalise",
+                message: "Would you like to capitalise each word?",
+                initial: true,
+              },
+              {
+                  type: "confirm",
+                  name: "addNumber",
+                  message: "Would you like to add a random number in the string?",
+                  initial: true,
+                },
+        ]);
 
-  const wordList = await getWordList();
-  const numberToJoin = response.addNumber ? Math.floor(Math.random() * 100) : undefined
-  const passphrase = await generatePassphrase(wordList, response.length, 4, response.separator, response.capitalise, numberToJoin)
-  console.log(passphrase, passphrase.length)
+        if (!options.length) {
+            console.log("Operation cancelled");
+            process.exit(1);
+        }
+    } else {
+        const opts = program.opts();
+        options = {
+            length: parseInt(opts.length),
+            separator: opts.separator,
+            capitalise: opts.capitalise === 'true',
+            addNumber: opts.addNumber === 'true'
+        };
+    }
+
+    const wordList = await getWordList();
+    const numberToJoin = options.addNumber ? Math.floor(Math.random() * 100) : undefined
+    const passphrase = await generatePassphrase(wordList, options.length, 4, options.separator, options.capitalise, numberToJoin)
+    console.log(passphrase, passphrase.length)
 }
 
 main().catch((err) => {
